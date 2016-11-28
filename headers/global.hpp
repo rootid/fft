1f1975fe94c75a83aac415e392b8dc8c0ceaadf8
rootid <vsinhsawant@gmail.com>
2016-11-27 23:02:44 -0500

    [master] updted global

--- headers/global.hpp
+++ headers/global.hpp
@@ -4,8 +4,11 @@
 #include<vector>
 #include<string>
 #include<algorithm>
+#include<sstream>
 #include<unordered_map>
 #include<limits.h>
+#include<cstring>
+#include<memory>
 // uncomment to disable assert()
 // #define NDEBUG
 #include <cassert>
