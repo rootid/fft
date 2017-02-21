92481264cba4d36024b9639c368d1c0fccd70f68
rootid <vsinhsawant@gmail.com>
2017-02-21 08:21:48 -0500

    [master] added graph

---
+++ graph/course-schedule-ii.cpp
@@ -0,0 +1,47 @@
+//Course Schedule II
+//There are a total of n courses you have to take, labeled from 0 to n - 1.
+//Some courses may have prerequisites, for example to take course 0 you have to first take course 1, which is expressed as a pair: [0,1]
+//Given the total number of courses and a list of prerequisite pairs, return the ordering of courses you should take to finish all courses.
+//There may be multiple correct orders, you just need to return one of them. If it is impossible to finish all courses, return an empty array.
+//For example:
+//2, [[1,0]]
+//There are a total of 2 courses to take. To take course 1 you should have finished course 0. So the correct course order is [0,1]
+//4, [[1,0],[2,0],[3,1],[3,2]]
+//There are a total of 4 courses to take. To take course 3 you should have finished both courses 1 and 2. Both courses 1 and 2 should be taken after you finished course 0. So one correct course order is [0,1,2,3]. Another correct ordering is[0,2,1,3].
+//Note:
+//The input prerequisites is a graph represented by a list of edges, not adjacency matrices. Read more about how a graph is represented.
+//You may assume that there are no duplicate edges in the input prerequisites
+
+vector<int> findOrder(int numCourses, vector<pair<int, int>>& prerequisites) { 
+  vector<vector<int> > adjList( numCourses,vector<int>() );
+	vector<int> inDegrees(numCourses,0);
+	vector<int> pOrderResult;
+	int sum = 0;
+	for (auto i : prerequisites) { 
+    adjList[i.second].push_back(i.first);
+		inDegrees[i.first]++;
+	}
+	stack<int> zeroInDegStack;
+	for(auto i=0;i<inDegrees.size();i++){ 
+	    if(inDegrees[i] == 0) { 
+	        zeroInDegStack.push(i); 
+	    }
+  }
+  if(zeroInDegStack.size() == 0) { 
+    return pOrderResult;
+  }
+  while(!zeroInDegStack.empty()) { 
+    pOrderResult.push_back(zeroInDegStack.top());
+    zeroInDegStack.pop();
+    for (auto j : adjList[pOrderResult.back()]) {
+        inDegrees[j]--;
+        if (inDegrees[j] == 0) {
+            zeroInDegStack.push(j); 
+        }
+    } 
+  } 
+  if(pOrderResult.size() == numCourses) {
+	    return pOrderResult;
+  } 
+  return vector<int>{}; 
+}
---
+++ graph/course-schedule.cpp
@@ -0,0 +1,111 @@
+//Course Schedule
+//There are a total of n courses you have to take, labeled from 0 to n - 1.
+//Some courses may have prerequisites, for example to take course 0 you have to first take course 1, which is expressed as a pair: [0,1]
+//Given the total number of courses and a list of prerequisite pairs, is it possible for you to finish all courses?
+//For example:
+//2, [[1,0]]
+//There are a total of 2 courses to take. To take course 1 you should have finished course 0. So it is possible.
+//2, [[1,0],[0,1]]
+//There are a total of 2 courses to take. To take course 1 you should have finished course 0, and to take course 0 you should also have finished course 1. So it is impossible.
+//Note:
+//The input prerequisites is a graph represented by a list of edges, not adjacency matrices. Read more about how a graph is represented.
+//You may assume that there are no duplicate edges in the input prerequisites.
+//Kahn's Algo
+//http://www.geeksforgeeks.org/topological-sorting-indegree-based-solution/
+//############################## Kahn's algo ############################## 
+bool canFinish(int numCourses, vector<pair<int, int> > &prerequisites) {
+  vector<vector<int> > adjList( numCourses,vector<int>() );
+	vector<int> inDegrees(numCourses,0);
+	int sum = 0;
+	for (auto i : prerequisites) { 
+    adjList[i.second].push_back(i.first);
+		inDegrees[i.first]++;
+	}
+	stack<int> zeroInDegStack;
+	for(auto i=0;i<inDegrees.size();i++){ 
+	    if(inDegrees[i] == 0) { 
+	        zeroInDegStack.push(i); 
+	    }
+  }
+  if(zeroInDegStack.size() == 0) { 
+    return false;
+  }
+  stack<int> pOrderResult;
+  while(!zeroInDegStack.empty()) { 
+    pOrderResult.push(zeroInDegStack.top());
+    zeroInDegStack.pop();
+    for (auto j : adjList[pOrderResult.top()]) {
+        inDegrees[j]--;
+        if (inDegrees[j] == 0) {
+            zeroInDegStack.push(j); 
+        }
+    } 
+  }
+  if(pOrderResult.size() != numCourses) {
+	    return false;
+  }
+  return true;
+}
+
+//######################################### BFS ############################## 
+bool canFinish(int numCourses, vector<pair<int, int> > &prerequisites) {
+		vector<unordered_set<int> > dg(numCourses);
+    for(auto &i:prerequisites) {
+			dg[i.second].insert(i.first);
+    }
+		vector<int> indegree(numCourses,0);
+		for (int i = 0; i != numCourses; i++) {
+			for(auto it = dg[i].begin(); it != dg[i].end(); it++) {
+				indegree[*it]++;
+      }
+    }
+		queue<int> zeros;
+		for (int i = 0; i != numCourses; ++i) { 
+      if (indegree[i] == 0) {
+				zeros.push(i);
+      } 
+    }
+    while (!zeros.empty()) { 
+      int seq = zeros.front();
+		  zeros.pop();
+		  for (auto it = dg[seq].begin(); it != dg[seq].end(); ++it) {
+		  	if (--indegree[*it] == 0) {
+		  		zeros.push(*it);
+        } 
+      } 
+      --numCourses;
+    } 
+    return numCourses == 0; 
+}
+
+//#################################### DFS ################################### 
+//Deepness will cause the issue (0->1->2->4->........-> 1)
+bool canFinish(int numCourses, vector<pair<int, int>>& prer) {
+    vector<vector<int> > dig(numCourses);
+    vector<int> visited(numCourses, 0); // White at initialization
+    for (int i = 0; i < prer.size(); i++) {
+        dig[prer[i].second].push_back(prer[i].first);
+    }
+    bool cycle = false;
+    for (int i = 0; i < numCourses; i++) {
+        if (cycle) return false;
+        if (visited[i] == 0) {
+          dfs_top(i, dig, visited, cycle);
+        }
+    }
+    return !cycle;
+}
+void dfs_top(int node, vector<vector<int>> &dig, vector<int> &visited, bool &cycle) {
+    if (visited[node] == 1) {
+      cycle = true; 
+      return;
+    } // cycle occurs, break the dfs chain and all return
+    visited[node] = 1; //Gray, searching
+    for (int i = 0; i < dig[node].size(); i++) {
+        dfs_top(dig[node][i], dig, visited, cycle);
+        if (cycle) {
+          return; // do some pruning here
+        }
+    }
+    visited[node] = 2; //Black Once finished.
+}
