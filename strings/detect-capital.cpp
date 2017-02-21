caa09485ca460a5af060d297e1a7764641ae2dec
rootid <vsinhsawant@gmail.com>
2017-02-21 08:59:57 -0500

    [master] added str

---
+++ strings/detect-capital.cpp
@@ -0,0 +1,40 @@
+//Given a word, you need to judge whether the usage of capitals in it is right or not.
+//We define the usage of capitals in a word to be right when one of the following cases holds:
+//All letters in this word are capitals, like "USA".
+//All letters in this word are not capitals, like "leetcode".
+//Only the first letter in this word is capital if it has more than one letter, like "Google".
+//Otherwise, we define that this word doesn't use capitals in a right way.
+//Example 1:
+//Input: "USA"
+//Output: True
+//Example 2:
+//Input: "FlaG"
+//Output: False
+//Note: The input will be a non-empty word consisting of uppercase and lowercase latin letters.
+//
+#include "../headers/global.hpp"
+
+
+//def detectCapitalUse(self, word):
+//    return word.isupper() or word.islower() or word.istitle()
+
+bool isCapital(char c) { 
+  return ( 'A' <= c && c <= 'Z'); 
+}
+
+
+bool detectCapitalUse(string word) { 
+  int numCaps = 0; 
+  for (auto &c : word) {
+    if (isCapital(c)) {
+      numCaps++;
+    }
+  } 
+  return ( numCaps == word.size() || ( numCaps == 1 && isCapital(word[0]) ) || numCaps==0 ); 
+}
+
+int main() {
+  //string t = "FlaG";
+  string t = "USA";
+  detectCapitalUse(t);
+}
