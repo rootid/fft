fb09f26cd1031e5a6aec28169fb3cd6ade2e1ac5
rootid <vsinhsawant@gmail.com>
2017-03-06 08:24:21 -0500

    [master] added distinct subse

--- dp/distinct-subsequences.cpp
+++ dp/distinct-subsequences.cpp
@@ -5,7 +5,38 @@
 //S = "rabbbit", T = "rabbit"
 //Return 3.
 
-//
-int numDistinct(string s, string t) {
-    
+//########################### DP ########################### 
+int numDistinct(string s, string t) { 
+    int m = t.length(), n = s.length();
+    vector<vector<int>> dp(m + 1, vector<int> (n + 1, 0));
+    //t is empty
+    for (int j = 0; j <= n; j++) {
+        dp[0][j] = 1;
+    }
+    for (int j = 1; j <= n; j++) { //iterate over s
+        for (int i = 1; i <= m; i++) { //iterate over t
+
+            dp[i][j] = dp[i][j - 1] + (t[i - 1] == s[j - 1] ? dp[i - 1][j - 1] : 0);
+        }
+    }
+    return dp[m][n];
 }
+
+//############################ Space optimized ############################ 
+int numDistinct(string s, string t) { 
+    int m = t.length(), n = s.length();
+    vector<int> cur(m + 1, 0);
+    cur[0] = 1;
+    for (int j = 1; j <= n; j++) { 
+        int pre = 1;
+        for (int i = 1; i <= m; i++) {
+            int temp = cur[i];
+            cur[i] = cur[i] + (t[i - 1] == s[j - 1] ? pre : 0);
+            pre = temp;
+        }
+    }
+    return cur[m];
+}
+
+
+/* vim: set ts=4 sw=4 sts=4 tw=120 et: */
