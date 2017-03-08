896b1174f70459c068f705305bf41c88f7b2b3ae
rootid <vsinhsawant@gmail.com>
2017-03-08 08:36:57 -0500

    [master] 2-pointer solution

--- 2-pointer/k-diff-pairs-in-an-array.cpp
+++ 2-pointer/k-diff-pairs-in-an-array.cpp
@@ -62,3 +62,24 @@ int findPairs(vector<int>& nums, int k) {
   }
   return cnt; 
 }
+
+
+//################################# With HashMap ################################# 
+int findPairs(vector<int>& nums, int k) { 
+  int ans = 0;
+  if(nums.size() == 0 || k < 0)
+      return ans;
+  unordered_map<int,int> dict;
+  for(int i = 0; i < nums.size(); ++ i)
+      ++ dict[nums[i]];
+  unordered_map<int,int>::iterator it = dict.begin();
+  for(; it != dict.end(); ++ it){
+      if(dict.find(it->first - k) != dict.end()){
+          if(k != 0)
+              ++ ans;
+          else if(it->second > 1)
+              ++ ans;
+      }
+  }
+  return ans; 
+}
