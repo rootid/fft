bdb4b12574221d1d49ecb3ed44904e2feaa5e8e0
rootid <vsinhsawant@gmail.com>
2017-03-09 08:09:47 -0500

    [master] added robber

---
+++ dp/house-robber.cpp
@@ -0,0 +1,54 @@
+//House Robber
+//You are a professional robber planning to rob houses along a street. Each house has a certain amount of money stashed, the only constraint stopping you from robbing each of them is that adjacent houses have security system connected and it will automatically contact the police if two adjacent houses were broken into on the same night.
+//Given a list of non-negative integers representing the amount of money of each house, determine the maximum amount of money you can rob tonight without alerting the police.
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+
+//This code handles odd-even overlapping [2,1,1,2] max=4
+int rob1(vector<int>&nums) {
+  int odd = 0;
+  int even = 0;
+  for(int i=0;i<nums.size();i++) {
+      if (i % 2 == 0) {
+        even = max ( odd ,even + nums[i]);
+      } else {
+        odd = max(even , odd + nums[i]);
+      }
+  }
+  return max(even,odd);
+}
+
+
+int rob(vector<int>&nums ) {
+  int len = nums.size();
+  vector<int> dp(len+1,0);
+  if(len == 0) {
+    return 0;
+  }
+  dp[1] = nums[0];
+  if(len == 1) {
+    return nums[0];
+  }
+  dp[2] = max(nums[0],nums[1]);
+  if(len == 2) {
+    return max(nums[0],nums[1]);
+  }
+  //cout << dp[2] << endl;
+  //cout << dp[len] << endl;
+  for(int i=3;i<=len;i++) {
+    //cout << " i " << dp[i] << endl;
+    dp[i] = max(dp[i-1] , dp[i-2] + nums[i-1]);
+    //cout << " after " << dp[i] << endl;
+  }
+  //cout << dp[len] << endl;
+  return dp[len];
+}
+
+int main() {
+  //vector<int> av( {0,0,0} );
+  vector<int> av( {1,1,1} );
+  rob(av);
+}
---
+++ trees/house-robber-iii.cpp
@@ -0,0 +1,74 @@
+//House Robber III
+//The thief has found himself a new place for his thievery again. There is only one entrance to this area, called the "root." Besides the root, each house has one and only one parent house. After a tour, the smart thief realized that "all houses in this place forms a binary tree". It will automatically contact the police if two directly-linked houses were broken into on the same night.
+//Determine the maximum amount of money the thief can rob tonight without alerting the police.
+//Example 1:
+//     3
+//    / \
+//   2   3
+//    \   \ 
+//     3   1
+//Maximum amount of money the thief can rob = 3 + 3 + 1 = 7.
+//Example 2:
+//     3
+//    / \
+//   4   5
+//  / \   \ 
+// 1   3   1
+//Maximum amount of money the thief can rob = 4 + 5 = 9.
+
+//############################### Recursive ############################### 
+int rob(TreeNode* root) { 
+  if (root == nullptr) 
+   return 0; 
+  int val = 0;
+  if (root->left != nullptr) {
+    val += rob(root->left->left) + rob(root->left->right); //pick first level LEFT tree node
+  }
+  if (root->right != nullptr) {
+      val += rob(root->right->left) + rob(root->right->right); //pick first level RIGHT tree node
+  }
+  return max(val + root->val, rob(root->left) + rob(root->right)); // get the max between root+first level and left+right subtree
+}
+
+//################## store cost of single node only ################## 
+int robHelper(TreeNode* root,unordered_map<TreeNode*,int>& umap) { 
+  if (root == nullptr) 
+   return 0; 
+  if (umap.count(root)) 
+   return umap[root];
+  int val = 0 ;
+  if (root->left != nullptr) {
+      val += robHelper(root->left->left, umap) + robHelper(root->left->right, umap);
+  }
+  if (root->right != nullptr) {
+      val += robHelper(root->right->left, umap) + robHelper(root->right->right, umap);
+  }
+  
+  val = max(val + root->val, robHelper(root->left, umap) + robHelper(root->right, umap));
+  umap[root] = val;
+  return val;
+}
+
+int rob(TreeNode* root) { 
+  unordered_map<TreeNode*,int> umap;
+  return robHelper(root,umap);
+}
+
+//############################### DFS solution + store the cost of 2 nodes ############################### 
+//res[0] = max Money allowing root to be stolen
+//res[1] = max Money avoiding root itself
+vector<int> dfs(TreeNode* x) {
+  if (!x)  { 
+    return {0,0};
+  }
+  vector<int> left = dfs(x->left);
+  vector<int> right = dfs(x->right);
+  vector<int> res {0,0};
+  res[0] = left[1] + right[1] + x->val;
+  res[1] = max(left[0], left[1]) + max(right[0], right[1]);
+  return res; 
+}
+int rob(TreeNode* root) { 
+  vector<int> num = dfs(root); 
+  return max(num[0], num[1]);
+}
