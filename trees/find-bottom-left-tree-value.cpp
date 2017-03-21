39d92c8b9e3c2704a5abc67928acdb909100a5c6
rootid <vsinhsawant@gmail.com>
2017-03-21 07:22:56 -0400

    [master] bottom left

--- trees/find-bottom-left-tree-value.cpp
+++ trees/find-bottom-left-tree-value.cpp
@@ -19,33 +19,57 @@
 //Output:
 //7
 //##################### DFS ##################### 
-int findBottomLeftValue(TreeNode* root) {
-    pair<int, int> res({0,root->val});
-    dfs(root, 0,res);
-    return res.second;
-}
-void dfs(TreeNode* root, int level,pair<int, int>& res){
+void dfsHelper(TreeNode* root, int level,pair<int, int>& res){
     if(root == nullptr) 
       return;
     if(level > res.first) {
         res.first = level;
         res.second = root->val;
     }
-    dfs(root->left, level+1,res);
-    dfs(root->right, level+1,res);
+    dfsHelper(root->left, level+1,res); //left->right
+    dfsHelper(root->right, level+1,res);
+}
+int findBottomLeftValue(TreeNode* root) {
+    pair<int, int> res({0,root->val});
+    dfsHelper(root, 0,res);
+    return res.second;
 }
 
-//##################### BFS ##################### 
+
+void findBottomLeftValue(TreeNode* root, int& maxDepth, int& leftVal, int depth) { 
+  if (root == NULL) { 
+    return; 
+  }
+  //Go to the left and right of each node 
+  findBottomLeftValue(root->left, maxDepth, leftVal, depth+1);
+  findBottomLeftValue(root->right, maxDepth, leftVal, depth+1);
+  
+  //Update leftVal and maxDepth
+  if (depth > maxDepth) {
+      maxDepth = depth;
+      leftVal = root->val;
+  } 
+}
+//Entry function
+int findBottomLeftValue(TreeNode* root) {
+    int maxDepth = 0;
+    //Initialize leftVal with root's value to cover the edge case with single node
+    int leftVal = root->val;
+    findBottomLeftValue(root, maxDepth, leftVal, 0);
+    return leftVal;
+}
+
+//##################### BFS from Right to left ##################### 
 int findBottomLeftValue(TreeNode* root) {
     queue<TreeNode*> queue;
     queue.push(root);
     while (!queue.empty()) {
         root = queue.front();
-        queue.pop();
+        queue.pop(); //1
         if (root->right != nullptr)
-            queue.push(root->right);
+            queue.push(root->right); //3 | 6 | 7
         if (root->left != nullptr)
-            queue.push(root->left);
+            queue.push(root->left); //3,2,6,5,4,7
     }
     return root->val;
 }
