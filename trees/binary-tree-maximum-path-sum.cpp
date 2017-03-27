c95df8820374914c85b61073a7294c80465982fa
rootid <vsinhsawant@gmail.com>
2017-03-27 08:09:21 -0400

    [master] max path sum

---
+++ trees/binary-tree-maximum-path-sum.cpp
@@ -0,0 +1,72 @@
+//Binary Tree Maximum Path Sum
+//Given a binary tree, find the maximum path sum.
+//For this problem, a path is defined as any sequence of nodes from some starting node to any node in the tree along the parent-child connections. The path must contain at least one node and does not need to go through the root.
+//For example:
+//Given the below binary tree,
+//       1
+//      / \
+//     2   3
+//Return 6.
+
+#include "../headers/global.hpp"
+#include "../headers/treenode.h"
+
+
+//maxValue = max(maxValue, left + right + node->val);
+//return max(left, right) + node->val;
+//maxValue is the value which recording whether this current root is the final root, 
+//so we use left + right + node.val. 
+//But to the //upper layer(after return statement), we cannot choose both left and right branches, so we need to select the larger one, so we use max(left, right) + node.val to prune the lower branch.
+
+int depth(TreeNode *root,int &res) { 
+  if(root == NULL) 
+    return 0;
+  int a = depth(root->left,res); 
+  int b = depth(root->right,res);
+  res = max(res,a+b+root->val);//if *root is the top node in the path (left+right+root)
+  return max(0,max(a, b)+root->val);//if *root is in the path, if this branch a burden or a plus (left/right + root)
+}
+
+int maxPathSum(TreeNode *root) { 
+  if(root == NULL) {
+    return 0;
+  }
+  res = root->val;
+  depth(root,res);
+  return res;
+}
+
+//######################################## Bottom-up solution I ######################################## 
+int maxPathSum(TreeNode *root) {
+    int max = numeric_limits<int>::min();
+    helper(root, max);
+    return max;
+}
+int helper(TreeNode *root, int& global_max) {
+    if (root == nullptr) return 0;
+    int l = max(0, helper(root->left, global_max));
+    int r = max(0, helper(root->right, global_max));
+    global_max = max(global_max, l + r + root->val); //Choose the current root with left and right subtree
+    return root->val + max(l, r); //Choose the either of left/right subtree
+}
+
+//######################################## Bottom-up solution II ######################################## 
+int helper(TreeNode *root, int &re) {
+    if (!root) return 0;
+    int l = helper(root->left, re);
+    int r = helper(root->right, re);
+    if (l < 0) l = 0;
+    if (r < 0) r = 0;
+    if (l + r + root->val > re) {
+      re = l + r + root->val; //update the max
+    }
+    //return root->val += max(l, r); //update the root value
+    return root->val + max(l, r); //return max root value
+}
+int maxPathSum(TreeNode *root) {
+    int max = numeric_limits<int>::min();
+    helper(root, max);
+    return max;
+}
+        
+
