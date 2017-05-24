ae7e116def41efe5c3ace32083ce5f39f8a55cb3
rootid <vsinhsawant@gmail.com>
2017-05-24 08:20:11 -0400

    [master] added sqaure + shorted conti subarray

---
+++ arrays/shortest-unsorted-continuous-subarray.cpp
@@ -0,0 +1,136 @@
+//Shortest Unsorted Continuous Subarray
+//Given an integer array, you need to find one continuous subarray that if you only sort this subarray in ascending order, then the whole array will be sorted in ascending order, too.
+//You need to find the shortest such subarray and output its length.
+//Example 1:
+//Input: [2, 6, 4, 8, 10, 9, 15]
+//Output: 5
+//Explanation: You need to sort [6, 4, 8, 10, 9] in ascending order to make the whole array sorted in ascending order.
+//Note:
+//Then length of the input array is in range [1, 10,000].
+//The input array may contain duplicates, so ascending order here means <=.
+//######################### Good O(log n) #########################  
+int findUnsortedSubarray(vector<int>& nums) { 
+  vector<int> sorted(nums);
+  sort(sorted.begin(), sorted.end());
+  int n = nums.size(), left = 0, right = n - 1;
+  while (left < n && nums[left] == sorted[left]) {
+      left++;
+  }
+  while (right > left && nums[right] == sorted[right]) {
+      right--;
+  }
+  return right + 1 - left;
+}
+
+//######################### Ugly O(log n) #########################  
+int findUnsortedSubarray(vector<int>& nums) {
+  vector<int> nums2(nums.begin(),nums.end());
+  sort(nums2.begin(),nums2.end());
+  int startIdx = 0;
+  int endIdx = 0;
+  int idx = 0;
+  int len = nums2.size();
+  while(idx < len) {
+    if(nums[idx] != nums2[idx]) {
+      startIdx = idx;
+      break;
+    }
+    idx++;
+  }
+  idx = len-1;
+  while(idx >= 0) {
+    if(nums[idx] != nums2[idx]) {
+      endIdx = idx;
+      break;
+    }
+    idx--;
+  }
+  return startIdx+endIdx-1;
+}
+
+//######################### T O(n) + S O(2n) #########################  
+int findUnsortedSubarray(vector<int>& nums) { 
+  int n = nums.size();
+  vector<int> maxlhs(n);   // max number from left to cur
+  vector<int> minrhs(n);   // min number from right to cur
+  for (int i = n - 1, minr = INT_MAX; i >= 0; i--) {
+    minrhs[i] = minr = min(minr, nums[i]);
+  }
+  for (int i = 0, maxl = INT_MIN; i < n;  i++) {
+    maxlhs[i] = maxl = max(maxl, nums[i]);
+  }
+  int i = 0, j = n - 1;
+  while (i < n && nums[i] <= minrhs[i]) {
+    i++;
+  }
+  while (j > i && nums[j] >= maxlhs[j]) { 
+    j--;
+  }
+  return j + 1 - i; 
+}
+
+//######################### O(n) #########################  
+//Space O(1)
+int findUnsortedSubarray(vector<int>& nums) {
+       int shortest = 0;
+       int left = 0, right = nums.size() - 1;
+       //find start left
+       while (left < nums.size() - 1 && nums[left] <= nums[left + 1]) { 
+         left++; 
+       }
+       //find start right
+       while (right > 0 && nums[right] >= nums[right - 1]) { 
+         right--; 
+       };
+       if (right > left) {
+         //find max and min value (vmax,vmin)
+         int vmin = INT_MAX, vmax = INT_MIN;
+         for (int i = left; i <= right; ++i) {
+             if (nums[i] > vmax) { 
+               vmax = nums[i]; 
+             }
+             if (nums[i] < vmin) { 
+               vmin = nums[i];
+             }
+         }
+         while (left >= 0 && nums[left] > vmin) { 
+           left--; 
+         };
+         while (right < nums.size() && nums[right] < vmax) { 
+           right++; 
+         };
+           
+           shortest = right - left - 1;
+       }
+       return shortest;
+}
+
+//######################### O(n) + S O(1)#########################  
+int findUnsortedSubarray(vector<int>& nums) { 
+  int n = nums.size();
+  if (n <= 1) return 0;
+  int left = 0, right = n - 1;
+  while (left < n - 1 && nums[left] <= nums[left + 1]) ++left;
+  if (left == n - 1) return 0; // return 0 if already sorted.
+  // Or we know the array is unsorted
+  // So, it's no need to judge right > 0
+  while (/*right > 0 && */nums[right] >= nums[right - 1]) --right;
+  int rmin = INT_MAX, lmax = INT_MIN;
+  for (int i = left; i <= right; ++i) {
+      if (nums[i] > lmax) lmax = nums[i];
+      if (nums[i] < rmin) rmin = nums[i];
+  }
+  while (left >= 0 && nums[left] > rmin) --left;
+  while (right < n && nums[right] < lmax) ++right;
+  return right - left - 1;
+}
+
+//############################ pytonic  ############################ 
+//def findUnsortedSubarray(self, A):
+//    B = sorted(A)
+//    if A == B: return 0
+//    for i in xrange(len(A)):
+//        if A[i] != B[i]: break
+//    for j in xrange(len(A)-1, -1, -1):
+//        if A[j] != B[j]: break
+//    return j - i + 1
---
+++ maths/valid-square.cpp
@@ -0,0 +1,118 @@
+//Valid Square
+//Given the coordinates of four points in 2D space, return whether the four points could construct a square.
+//The coordinate (x,y) of a point is represented by an integer array with two integers.
+//Example:
+//Input: p1 = [0,0], p2 = [1,1], p3 = [1,0], p4 = [0,1]
+//Output: True
+//Note:
+//All the input integers are in the range [-10000, 10000].
+//A valid square has four equal sides with positive length and four equal angles (90-degree angles).
+//Input points have no order.
+
+
+bool validSquare(vector<int>& p1, vector<int>& p2, vector<int>& p3, vector<int>& p4) {
+}
+
+//############################  combination ############################ 
+//NOTE : this solution will work as the problem is limited to integer coordinates.
+//equilateral triangle: Non integer case (0,0),(0,2),(-1,√3),(1,√3) : Ideally expected false,howerver in this case it returns true.
+
+//
+int d(vector<int>& p1, vector<int>& p2) {
+    return (p1[0] - p2[0]) * (p1[0] - p2[0]) + (p1[1] - p2[1]) * (p1[1] - p2[1]);
+}
+bool validSquare(vector<int>& p1, vector<int>& p2, vector<int>& p3, vector<int>& p4) {
+    unordered_set<int> s({ d(p1, p2), d(p1, p3), d(p1, p4), d(p2, p3), d(p2, p4), d(p3, p4) });
+    return !s.count(0) && s.size() == 2;
+}
+
+
+//################# pytonic ################# 
+//    def validSquare(self, p1, p2, p3, p4):
+//        """
+//        :type p1: List[int]
+//        :type p2: List[int]
+//        :type p3: List[int]
+//        :type p4: List[int]
+//        :rtype: bool
+//        """
+//        points = [p1, p2, p3, p4]
+//        return len({(a[0]-b[0])**2 + (a[1]-b[1])**2 for a in points for b in points}) == 3 and \
+//               len(set(map(tuple, points))) == 4
+//
+//
+//################# Iterators ################# 
+//class Solution(object):
+//    def validSquare(self, p1, p2, p3, p4):
+//        def D((P, Q)):
+//            return (P[0] - Q[0]) ** 2 + (P[1] - Q[1]) ** 2
+//        S = set(map(D, itertools.combinations((p1, p2, p3, p4), 2)))
+//        return len(S) == 2 and 0 not in S
+//
+//######################## Naive ######################## 
+//class Solution(object):
+//    def validSquare(self, p1, p2, p3, p4):
+//        points = [p1, p2, p3, p4]
+//        
+//        dists = collections.Counter()
+//        for i in range(len(points)):
+//            for j in range(i+1, len(points)):
+//                dists[self.getDistance(points[i], points[j])] += 1
+//        
+//        return len(dists.values())==2 and 4 in dists.values() and 2 in dists.values()
+//        
+//    def getDistance(self, p1, p2):
+//        return (p1[0] - p2[0])**2 + (p1[1] - p2[1])**2
+//
+//######################### Sort + No multiplication ######################### 
+//def validSquare(self, p1, p2, p3, p4):
+//    if p1 == p2 == p3 == p4: return False
+//
+//    p1,p2,p3,p4 = sorted([p1,p2,p3,p4])
+//    if p2[1] < p3[1]: p2,p3 = p3,p2
+//
+//    return p3 == [p1[0] + (p2[1]-p1[1]), p1[1] - (p2[0]-p1[0])]\
+//       and p4 == [p2[0] + (p2[1]-p1[1]), p2[1] - (p2[0]-p1[0])]
+//
+//
+//#########Any 3 points make up a Isosceles Right Triangle #########
+//
+//class Solution(object):
+//    def validSquare(self, p1, p2, p3, p4):
+//        """
+//        :type p1: List[int]
+//        :type p2: List[int]
+//        :type p3: List[int]
+//        :type p4: List[int]
+//        :rtype: bool
+//        """
+//        def distance(p1, p2):
+//            return (p1[0] - p2[0]) ** 2 + (p1[1] - p2[1]) ** 2
+//
+//        def foo(p1, p2, p3):
+//            d1, d2, d3 = sorted((distance(p1, p2), distance(p2, p3), distance(p3, p1)))
+//            return 0 < d1 == d2 == d3 / 2.0
+//
+//class Solution(object):
+//    def validSquare(self, p1, p2, p3, p4):
+//        """
+//        :type p1: List[int]
+//        :type p2: List[int]
+//        :type p3: List[int]
+//        :type p4: List[int]
+//        :rtype: bool
+//        """
+//        def distance(p1, p2):
+//            return (p1[0] - p2[0]) ** 2 + (p1[1] - p2[1]) ** 2
+//        def foo(p1, p2, p3):
+//            arr = [distance(p1, p2), distance(p2, p3), distance(p3, p1)]
+//            if 0 in arr:
+//                return False
+//            if arr[0] == arr[1] and 2 * arr[0] == arr[2]:
+//                return True
+//            if arr[1] == arr[2] and 2 * arr[1] == arr[0]:
+//                return True
+//            if arr[2] == arr[0] and 2 * arr[2] == arr[1]:
+//                return True
+//            return False
+//        return foo(p1, p2, p3) and foo(p1, p2, p4) and foo(p1, p3, p4) and foo(p2, p3, p4)
