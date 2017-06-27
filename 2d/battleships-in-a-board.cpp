8c4a4c058c926a12c1060bff6bb5a49d8e085e6f
rootid <vsinhsawant@gmail.com>
2017-06-27 08:26:43 -0400

    [master] max-product-3nos+battleship

---
+++ 2d/battleships-in-a-board.cpp
@@ -0,0 +1,131 @@
+//Battleships in a Board
+//Given an 2D board, count how many battleships are in it. The battleships are represented with 'X's, empty slots are represented with '.'s. You may assume the following rules:
+//You receive a valid board, made of only battleships or empty slots.
+//Battleships can only be placed horizontally or vertically. In other words, they can only be made of the shape 1xN (1 row, N columns) or Nx1 (N rows, 1 column), where N can be of any size.
+//At least one horizontal or vertical cell separates between two battleships - there are no adjacent battleships.
+//Example:
+//X..X
+//...X
+//...X
+//In the above board there are 2 battleships.
+//Invalid Example:
+//...X
+//XXXX
+//...X
+//This is an invalid board that you will not receive - as battleships will always have a cell separating between them.
+//Follow up:
+//Could you do it in one-pass, using only O(1) extra memory and without modifying the value of the board?
+
+//######################################### traversal O(mn) ######################################### 
+int countBattleships(vector<vector<char>>& board) { 
+  int result = 0;
+  if(board.empty()) {
+      return result;
+  }
+  for (int i = 0; i < board.size(); i++) {
+    for (int j = 0; j < board[0].size(); j++) {
+      //verify adjacent cells : must be empty
+      if (board[i][j] == 'X' && (i == 0 || board[i - 1][j] == '.') && (j == 0 || board[i][j - 1] == '.')) {
+        result++;
+      } 
+    } 
+  } 
+  return result; 
+}
+
+//######################################### traversal O(mn) ######################################### 
+int countBattleships(vector<vector<char>>& b) { 
+  int cnt = 0;
+  for (int i = 0; i < b.size(); ++i)
+    for (int j = 0; j < b[0].size(); ++j)
+      cnt += (b[i][j]-'.') && (!i||b[i-1][j]-'X') && (!j||b[i][j-1]-'X');
+  return cnt;
+}
+
+//######################################### DFS ######################################### 
+class Solution {
+public:
+    int m, n; 
+    vector<vector<bool>> flag;
+    int go[4][2] = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+
+    void dfs(vector<vector<char>>& board, int i, int j) {
+        if (i < 0 || i >= m || j < 0 || j >= n || board[i][j] == '.' || flag[i][j]) return;
+        flag[i][j] = true;
+        for (int d = 0; d < 4; ++d) dfs(board, i+go[d][0], j+go[d][1]);
+    }
+
+    int countBattleships(vector<vector<char>>& board) {
+        if (board.empty()) return 0;
+        m = board.size(), n = board[0].size();
+        flag.resize(m, vector<bool>(n, false));
+        int result = 0;
+        for (int i = 0; i < m; ++i)
+            for (int j = 0; j < n; ++j)
+                if (board[i][j] == 'X' && !flag[i][j]) {
+                    ++result;
+                    dfs(board, i, j);
+                }
+        return result;
+    }
+};
+
+//######################################### BFS ######################################### 
+class Solution {
+public:
+    int go[4][2] = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+    int countBattleships(vector<vector<char>>& board) {
+        if (board.empty()) return 0;
+        int m = board.size(), n = board[0].size();
+        vector<vector<bool>> flag(m, vector<bool>(n, false));
+        int result = 0;
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (board[i][j] == 'X' && !flag[i][j]) {
+                    ++result;
+
+                    queue<pair<int, int>> q;
+                    q.push({i, j});
+                    while (!q.empty()) {
+                        auto t = q.front(); q.pop();
+                        flag[t.first][t.second] = true;
+                        for (int d = 0; d < 4; ++d) {
+                            int ni = t.first+go[d][0], nj = t.second+go[d][1];
+                            if (ni < 0 || ni >= m || nj < 0 || nj >= n || board[ni][nj] == '.' || flag[ni][nj]) continue;
+                            q.push({ni, nj});
+                        }
+                    }
+                }
+            }
+        }
+        return result;
+    }
+};
+
+
+//######################################### pytonic ######################################### 
+//class Solution(object):
+//    def countBattleships(self, board):
+//        if len(board) == 0: return 0
+//        m, n = len(board), len(board[0])
+//        count = 0
+//        for i in range(m):
+//            for j in range(n):
+//                if board[i][j] == 'X' and (i == 0 or board[i-1][j] == '.') and (j == 0 or board[i][j-1] == '.'):
+//                    count += 1
+//        return count
+//
+//
+//def countBattleships(self, board):
+//    total = 0
+//    for i in range(len(board)):
+//        for j in range(len(board[0])):
+//            if board[i][j] == 'X':
+//                flag = 1
+//                if j > 0 and board[i][j-1] == 'X': flag = 0
+//                if i > 0 and board[i-1][j] == 'X': flag = 0
+//                total += flag
+//    return total
+
+
+// vim: set sw=2 sts=2 tw=120 et nospell : 
---
+++ arrays/maximum-product-of-three-numbers.cpp
@@ -0,0 +1,77 @@
+//Maximum Product of Three Numbers
+//Given an integer array, find three numbers whose product is maximum and output the maximum product.
+//Example 1:
+//Input: [1,2,3]
+//Output: 6
+//Example 2:
+//Input: [1,2,3,4]
+//Output: 24
+//Note:
+//The length of the given array will be in range [3,104] and all elements are in the range [-1000, 1000].
+//Multiplication of any three numbers in the input won't exceed the range of 32-bit signed integer.
+//
+
+//############################ pick always 2 (- * - = +) ############################ 
+//Either product of 3 biggest positive values will be maxProduct 
+//or if there are negative values then pick the 2 biggest negative values and multiply with biggest positive value
+int maximumProduct(vector<int>& nums) {
+    sort(nums.begin(), nums.end());
+    int n = nums.size();
+    int temp1 = nums[n-1]*nums[n-2]*nums[n-3];
+    int temp2 = nums[0]*nums[1]*nums[n-1];
+    return temp1>temp2?temp1:temp2;
+}
+
+/**
+ * when there are no negative, use top 3
+ * when there are 1 negative, use top 3
+ *   -1, 0, 1
+ *   -1, 0, 1, 2
+ *   -1, 0, 1, 2, 3
+ * when there are 2 negative, still use top 3, but the top 2 * bottom 1 might be bigger
+ *   -4, -3, 0, 1, 2, 3
+ *   -4, -3, 0
+ */
+class Solution {
+public:
+    int maximumProduct(vector<int>& a) {
+        sort(a.begin(), a.end());
+        int n = a.size();
+        int res = a[n - 3] * a[n - 2] * a[n - 1];
+        if (a[0] < 0 && a[1] < 0) {
+            res = max(res, a[0] * a[1] * a[n - 1]);
+        }
+        return res;
+    }
+};
+
+//############################ pytonic ############################ 
+//
+//class Solution(object):
+//    def maximumProduct(self, nums):
+//        """
+//        :type nums: List[int]
+//        :rtype: int
+//        """
+//        nums.sort()
+//        return max(nums[-1]*nums[-2]*nums[-3], nums[0]*nums[1]*nums[-1])
+
+//Sort the array. Any "middle" numbers not in the first 3 or last 3 cannot be used in the final answer. 
+//If we are using a middle number, it must have both a left-neighbor and a right-neighbor, and switching to one of these neighbors will increase the product.
+//def maximumProduct(self, A):
+//    A.sort()
+//    if len(A) > 6:
+//        A = A[:3] + A[-3:]
+//    
+//    return max(A[i] * A[j] * A[k]
+//    	       for i in xrange(len(A))
+//    	       for j in xrange(i+1, len(A))
+//    	       for k in xrange(j+1, len(A)))
+//
+//
+//def maximumProduct(self, A):
+//    A.sort()
+//    del A[3:-3]
+//    return max(a * b * c for a, b, c in itertools.combinations(A, 3))
+
+// vim: set sw=2 sts=2 tw=120 et nospell : 
