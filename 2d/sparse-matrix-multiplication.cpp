d4653dc018656a26f49f0c8ddc9f45d5b2883939
rootid <vsinhsawant@gmail.com>
2017-08-08 12:11:44 +0000

    updated sparse

--- 2d/sparse-matrix-multiplication.cpp
+++ 2d/sparse-matrix-multiplication.cpp
@@ -93,3 +93,24 @@ def multiply(self, A, B):
     return [[sum(row[j]*b for j, b in col)
              for col in cols]
             for row in A]
+
+
+//######################################### Pytonic + Sparse matrix######################################### 
+class Solution(object):
+    def multiply(self, A, B):
+        """
+        :type A: List[List[int]]
+        :type B: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        if A is None or B is None: return None
+        m, n, l = len(A), len(A[0]), len(B[0])
+        if len(B) != n:
+            raise Exception("A's column number must be equal to B's row number.")
+        C = [[0 for _ in range(l)] for _ in range(m)]
+        for i, row in enumerate(A):
+            for k, eleA in enumerate(row):
+                if eleA:
+                    for j, eleB in enumerate(B[k]):
+                        if eleB: C[i][j] += eleA * eleB
+        return C
