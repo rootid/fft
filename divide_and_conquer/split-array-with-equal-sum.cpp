5f89b94fd5e8f3e27827b46149f55e835e13549a
rootid <vsinhsawant@gmail.com>
2017-08-28 13:37:12 +0000

    celibrity + split array

---
+++ divide_and_conquer/split-array-with-equal-sum.cpp
@@ -0,0 +1,215 @@
+//Split Array with Equal Sum
+//Given an array with n integers, you need to find if there are triplets (i, j, k) which satisfies following conditions:
+//0 < i, i + 1 < j, j + 1 < k < n - 1
+//Sum of subarrays (0, i - 1), (i + 1, j - 1), (j + 1, k - 1) and (k + 1, n - 1) should be equal.
+//where we define that subarray (L, R) represents a slice of the original array starting from the element indexed L to the element indexed R.
+//Example:
+//Input: [1,2,1,2,1,2,1]
+//Output: True
+//Explanation:
+//i = 1, j = 3, k = 5. 
+//sum(0, i - 1) = sum(0, 0) = 1
+//sum(i + 1, j - 1) = sum(2, 2) = 1
+//sum(j + 1, k - 1) = sum(4, 4) = 1
+//sum(k + 1, n - 1) = sum(6, 6) = 1
+//Note:
+//1 <= n <= 2000.
+//Elements in the given array will be in range [-1,000,000, 1,000,000].
+
+//L ------------- sum(0, i - 1) = sum(0, 0) = 1
+//L------------- sum(i + 1, j - 1) = sum(2, 2) = 1
+//
+//R------------- sum(j + 1, k - 1) = sum(4, 4) = 1
+//R------------- sum(k + 1, n - 1) = sum(6, 6) = 1
+
+//################################################### O(N^2) ################################################### 
+// j is used for middle cut, i for left cut and k for right cut.
+// 0 ->>>>>>> j  <<<<<<<<<<- n-1
+//	  i--j-1--j--j+2-----n-1
+//
+//Here j is used for middle cut, i for left cut and k for right cut.
+//Iterate middle cuts and then find left cuts which divides the first half into two equal quarters,
+//store that quarter sums in the hashset.
+//Then find right cuts which divides the second half into two equal quarters and check if quarter sum is present in the hashset. If yes return true.
+public boolean splitArray(int[] nums) { 
+	if (nums.length < 7) 
+		return false;
+    int[] sum = new int[nums.length];
+    sum[0] = nums[0];
+    for (int i = 1; i < nums.length; i++) {
+        sum[i] = sum[i - 1] + nums[i]; //store sum from i = o to n
+    }
+    for (int j = 3; j < nums.length - 3; j++) { 
+        HashSet < Integer > set = new HashSet < > ();
+        for (int i = 1; i < j - 1; i++) {
+            if (sum[i - 1] == sum[j - 1] - sum[i]) //(0,i-1) == (i+1,j-1) -- I
+                set.add(sum[i - 1]);
+        }
+        for (int k = j + 2; k < nums.length - 1; k++) { //why not k = j+1
+            if (sum[nums.length - 1] - sum[k] == sum[k - 1] - sum[j] && set.contains(sum[k - 1] - sum[j])) //(k+1,n-1) = (j+1,k-1) && ( (j+1,k-1) == I )
+                return true;
+        }
+    }
+    return false;
+  }
+}
+
+
+
+//################################################### Backtracking ################################################### 
+//O(n^3)
+//Just think this problem as a DFS. What we need is to search for 3 positions (i, j, k) 
+//and see if they divide the array to 4 parts with same summary. Some tricks:
+//Calculate left on the fly. Thus at last we don't need to calc summary of the 4th part.
+//Skip 0 during calculate target because adding zero won't change it.
+public class Solution {
+    public boolean splitArray(int[] nums) {
+        int sum = 0, target = 0;
+        for (int num : nums) sum += num;
+        for (int i = 1; i + 5 < nums.length; i++) {
+            if (i != 1 && nums[i - 1] == 0  && nums[i] == 0) continue;
+            target += nums[i - 1];
+            if (dfs(nums, i + 1, target, sum - target - nums[i], 1)) return true;
+        }
+        return false;
+    }
+    
+    private boolean dfs(int[] nums, int start, int target, int left, int depth) {
+        if (depth == 3) {
+            if (left == target) return true;
+            return false;
+        }
+        
+        int sub = 0;
+        for (int j = start + 1; j + 5 - depth * 2 < nums.length; j++) {
+            sub += nums[j - 1];
+            if (sub == target) {
+                if (dfs(nums, j + 1, target, left - sub - nums[j], depth + 1)) {
+                    return true;
+                }
+            }
+        }
+        
+        return false;
+    }
+}
+
+//################################################### python ################################################### 
+//Let A be the array. As in most problems involving querying the sum of contiguous elements of an array,
+//let P[x] = sum(A[:x]) be the prefix sums of A, which can be found in linear time.
+//Then the sums in question are P[i] = P[j] - P[i+1] = P[k] - P[j+1] = P[-1] - P[k+1].
+//For every j < k, P[i] = P[-1] - P[k+1] is a necessary requirement to choose i,
+//so let's iterate over those indices first. This gives us the advantage that since we are iterating over a sorted list of candidate indices i, we can break when i >= j.
+def splitArray(self, A):
+    P = [0]
+    for x in A: P.append(P[-1] + x)
+    
+    N = len(A)
+    Pinv = collections.defaultdict(list)
+    for i, u in enumerate(P):
+        Pinv[u].append(i)
+        
+    for j in xrange(1, N-1):
+        for k in xrange(j+1, N-1):
+            for i in Pinv[P[-1] - P[k+1]]:
+                if i >= j: break
+                if P[i] == P[j] - P[i+1] == P[k] - P[j+1]:
+                    return True
+    return False
+
+
+//#########################################  Divide and Conquer ######################################### 
+//First, for every middle point j, we split nums into two subarray nums[:j] and nums[j+1:].
+//In the helper function split, try to remove one element from the subarray,
+//if the the sums of two remaining left and right sub-subarray are equal, we keep the sum of sub-subarray in the set we return.
+//Once we have any intersection between the two sets, we know we can make it.
+//Keep in mind len(nums) > 6 is a must since we need to split original array into four parts.
+//like divide and conquer (only twice though).
+class Solution(object):
+    def splitArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        def split(A):
+            total = sum(A)
+            for i in range(1, len(A)): A[i] += A[i-1]
+            return {A[i-1] for i in range(1, len(A)-1) if A[i-1] == total - A[i]}
+            
+        return len(nums) > 6 and any(split(nums[:j]) & split(nums[j+1:]) \
+                             for j in range(3, len(nums)-3))
+
+//#########################################  Divide and Conquer + Optimized ######################################### 
+def splitArray(self, nums):
+        n = len(nums)
+        s = [0] * (n + 1)
+        for i in range(n): s[i + 1] = s[i] + nums[i]
+        def check(l, r):
+            return set(s[m] - s[l] for m in range(l + 1, r + 1) if s[m] - s[l] == s[r + 1] - s[m + 1])
+        return any(check(0, j - 1) & check(j + 1, n - 1) for j in range(n))
+	
+
+//################################################### CPP version  ################################################### 
+class Solution {
+public:
+    bool splitArray(vector<int>& nums) {
+        vector<int> sms;
+        sms.push_back(0);
+        int N = nums.size();
+        
+        for(auto i : nums) {
+            sms.push_back(sms[sms.size()-1] + i);
+            
+        }
+        
+        unordered_map<int, vector<int>> hsh;
+        
+        for(int i = 0; i < sms.size(); i++) {
+            hsh[sms[i]].push_back(i);
+        }
+        
+        for(int i = 1; i < N - 1; i++ ) {
+            for(int j = i + 1; j < N -1 ; j++) {
+                for(auto f: hsh[sms[sms.size()-1] - sms[j+1]]) {
+                    if(f >= i) break;
+                    if(sms[f] == sms[i] - sms[f+1] && sms[f] == sms[j] - sms[i+1]) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+};
+
+//################################################### O(n^3) ################################################### 
+class Solution {
+public:
+    bool splitArray(vector<int>& nums) {
+        vector<int> sum;
+        int prev = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] == 0 && i > 0 && nums[i - 1] == 0)  continue; // skip extra 0s to accelerate
+            prev += nums[i];
+            sum.push_back(prev);
+        }
+
+        int n = sum.size();
+        for (int i = 1; i < n; i++) {
+            int sum1 = sum[i - 1];
+            for (int j = i + 2; j < n - 3; j++) {
+                int sum2 = sum[j - 1] - sum[i];
+                if (sum2 != sum1) continue;
+                for (int k = j + 2; k < n - 1; k++) {
+                    int sum3 = sum[k - 1] - sum[j];
+                    int sum4 = sum[n - 1] - sum[k];
+                    if (sum2 == sum3 && sum3 == sum4) {
+                        return true;
+                    }
+                }
+            }
+        }
+
+        return false;
+    }
+};
---
+++ stack/find-the-celebrity.cpp
@@ -0,0 +1,97 @@
+//Find the Celebrity
+//Suppose you are at a party with n people (labeled from 0 to n - 1) and among them, there may exist one celebrity. The definition of a celebrity is that all the other n - 1 people know him/her but he/she does not know any of them.
+//Now you want to find out who the celebrity is or verify that there is not one. The only thing you are allowed to do is to ask questions like: "Hi, A. Do you know B?" to get information of whether A knows B. You need to find out the celebrity (or verify there is not one) by asking as few questions as possible (in the asymptotic sense).
+//You are given a helper function bool knows(a, b) which tells you whether A knows B. Implement a function int findCelebrity(n), your function should minimize the number of calls to knows.
+//Note: There will be exactly one celebrity if he/she is in the party. Return the celebrity‘s label if there is a celebrity in the party. If there is no celebrity, return -1.
+
+//######################################### Stack approach ######################################### 
+// TC : O(n) , SC : O(n)
+public int findCelebrity(int n) {
+    // base case
+    if (n <= 0) return -1;
+    if (n == 1) return 0; //only 1 persion in the party
+    Stack<Integer> stack = new Stack<>();
+    // put all people to the stack
+    for (int i = 0; i < n; i++) stack.push(i);
+    int a = 0, b = 0;
+    while (stack.size() > 1) {
+        a = stack.pop();
+		b = stack.pop();
+        if (knows(a, b)) //b : famous/celibrity
+            // a knows b, so a is not the celebrity, but b may be
+            stack.push(b);
+        else 
+            // a doesn't know b, so b is not the celebrity, but a may be
+            stack.push(a);
+    }
+    // double check the potential celebrity
+    int c = stack.pop();
+    for (int i = 0; i < n; i++) {
+        // c should not know anyone else
+        if (i != c && (knows(c, i) || !knows(i, c)))
+            return -1;
+	}
+    return c;
+}
+
+//#########################################  Constant space #########################################  
+//TC : O(3n-1)
+//SP : O(1)
+int findCelebrity(int n) {
+    if(n<2) return n ? 0 : -1;
+    int curr = 0, next = 1;
+    for(;next<n;next++) 
+		if (knows(curr,next))  //next might be a celibrity
+			curr = next;
+    for(int i=0; i<n; i++) if(curr != i && (knows(curr, i) || !knows(i, curr))) return -1;
+    return curr;
+}
+
+//#########################################  2 pointer solution #########################################  
+public int findCelebrity(int n) {
+    if (n < 0) return 0;
+    int l = 0, r = n - 1;
+    while (l < r) {
+        if (knows(l, r)) l++;
+        else r--;
+    }
+    int i = 0;
+    while (i < n) {
+        if (i != l && (knows(l, i) || !knows(i, l))) return -1;
+        i++;
+    }
+    return l;
+}
+
+
+
+//The first loop is to exclude n - 1 labels that are not possible to be a celebrity.
+//After the first loop, x is the only candidate.
+//The second and third loop is to verify x is actually a celebrity by definition.
+//The key part is the first loop. To understand this you can think the knows(a,b) as a a < b comparison, if a knows b then a < b, if a does not know b, a > b.
+//Then if there is a celebrity, he/she must be the "maximum" of the n people.
+//However, the "maximum" may not be the celebrity in the case of no celebrity at all.
+//Thus we need the second and third loop to check if x is actually celebrity by definition.
+//The total calls of knows is thus 3n at most. One small improvement is that in the second loop we only need to check i in the range [0, x). You can figure that out yourself easily.
+
+public class Solution extends Relation {
+     public int findCelebrity(int n) {
+        int x = 0;
+        for (int i = 0; i < n; ++i) if (knows(x, i)) x = i; // x is celibrity
+        for (int i = 0; i < x; ++i) if (knows(x, i)) return -1;
+        for (int i = 0; i < n; ++i) if (!knows(i, x)) return -1;
+        return x;
+    }
+}
+
+
+def findCelebrity(self, n):
+    x = 0
+    for i in xrange(n):
+        if knows(x, i):
+            x = i
+    if any(knows(x, i) for i in xrange(x)):
+        return -1
+    if any(not knows(i, x) for i in xrange(n)):
+        return -1
+    return x
